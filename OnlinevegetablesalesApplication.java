674b31e487ab19c2086f2fb7702628c565ef1656
SiddhiShirdhankar <98864931+SiddhiShirdhankar@users.noreply.github.com>
2022-02-02 11:43:27 +0530

    Add files via upload

---
+++ OnlinevegetablesalesApplication.java
@@ -0,0 +1,13 @@
+package com.ovs.java.onlinevegetablesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinevegetablesalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinevegetablesalesApplication.class, args);
+	}
+
+}
